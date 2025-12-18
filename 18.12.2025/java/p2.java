a4a2453ce4272a2c03864024fa4116511f05a93e
Mahasri-V <151143352+Mahasri-V@users.noreply.github.com>
2025-12-18 21:04:30 +0530

    p2.java

---
+++ 18.12.2025/java/p2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String w : words) {
+            cnt.merge(w, 1, Integer::sum);
+        }
+        int m = s.length(), n = words.length;
+        int k = words[0].length();
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < k; ++i) {
+            Map<String, Integer> cnt1 = new HashMap<>();
+            int l = i, r = i;
+            int t = 0;
+            while (r + k <= m) {
+                String w = s.substring(r, r + k);
+                r += k;
+                if (!cnt.containsKey(w)) {
+                    cnt1.clear();
+                    l = r;
+                    t = 0;
+                    con
